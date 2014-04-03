ffda37016ba8bee9ace624b843c5cd302f722dd5
Eric Berry <cavneb@gmail.com>
2014-04-03 10:31:03 -0600

    added migration tie-in

--- lib/lti_box_engine/engine.rb
+++ lib/lti_box_engine/engine.rb
@@ -5,5 +5,13 @@ module LtiBoxEngine
     config.generators do |g|
       g.test_framework :rspec
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s == root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
